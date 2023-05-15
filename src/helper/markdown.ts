bd55571df81b9e708d99868be952e30b02a50318
ujklm23 <ridwanrasyid345@gmail.com>
2023-05-15 11:17:31 +0700

    repair: post order

--- src/helper/markdown.ts
+++ src/helper/markdown.ts
@@ -14,24 +14,26 @@ export const getAllFrontmatter = (): Post[] => {
   const files = fs.readdirSync(path.join('src/posts')).filter(file => file.split('.')[1] === 'mdx');
 
   // Iterates of file
-  const posts: Post[] = files.map(file => {
-    // Extract to matter format
-    const { data, content } = matter(fs.readFileSync(path.join('src/posts', file), 'utf-8'));
+  const posts: Post[] = files
+    .map(file => {
+      // Extract to matter format
+      const { data, content } = matter(fs.readFileSync(path.join('src/posts', file), 'utf-8'));
 
-    return {
-      slug: file.split('.')[0],
-      readingTime: readingTime(content),
-      data: {
-        title: data.title,
-        publishedAt: data.publishedAt,
-        draft: data.draft,
-        excerpt: data.excerpt,
-        tags: data.tags,
-      },
-      source: { compiledSource: '' },
-      content,
-    };
-  });
+      return {
+        slug: file.split('.')[0],
+        readingTime: readingTime(content),
+        data: {
+          title: data.title,
+          publishedAt: data.publishedAt,
+          draft: data.draft,
+          excerpt: data.excerpt,
+          tags: data.tags,
+        },
+        source: { compiledSource: '' },
+        content,
+      };
+    })
+    .sort((a, b) => new Date(b.data.publishedAt) - new Date(a.data.publishedAt));
 
   // Return formated posts
   return posts;
