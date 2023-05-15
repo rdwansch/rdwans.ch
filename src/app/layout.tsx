5de3b4ab444418ad47a444dea0db635d5728f3c5
ujklm23 <ridwanrasyid345@gmail.com>
2023-05-15 11:18:59 +0700

    fix: theme and stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   // }, []);
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <body>
         {/* {mounted ? ( */}
         {/* <Provider> */}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -35,7 +35,7 @@ export default function Nav() {
           </li>
           <li className="flex items-center">
             <button type="button" onClick={() => setIsDark(prev => !prev)}>
-              {!isDark ? (
+              {isDark ? (
                 <svg className="h-5 w-5 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path
                     strokeLinecap="round"
