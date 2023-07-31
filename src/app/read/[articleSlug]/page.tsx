d0533c2d25cd9a99c42853d41a23e6240d0fabad
ujklm23 <ridwanrasyid345@gmail.com>
2023-07-31 10:01:06 +0700

    post: jamstack

--- src/app/read/[articleSlug]/page.tsx
+++ src/app/read/[articleSlug]/page.tsx
@@ -22,46 +22,49 @@ export default async function page(props: Props) {
   // console.log(res);
 
   return (
-    <div className="article-container mt-10">
-      <div>
-        <h1
-          className={
-            `${roboto.className} dark:text-gray-300 text-center text-3xl sm:text-4xl md:text-5xl text-gray-900 relative mt-10 ` +
-            "after:content[''] after:absolute after:w-[200px] after:h-[180px] after:-bottom-20 after:left-32 after:right-0 after:mx-auto " +
-            'after:rounded-full ' +
-            "after:bg-cover after:bg-[url('/gradient.svg')]"
-          }
-        >
-          {res.data.title}
-        </h1>
-        <p className="font-bold text-center text-violet-600 dark:text-violet-600 mb-10">{res.data.tags.join('  ')}</p>
-        <span className="dark:text-gray-400 text-gray-600">
-          {res.data.publishedAt} &bull; {res.readingTime} min
-        </span>
-        <hr className="dark:border-gray-600 border-gray-300 mt-2" />
-      </div>
-      <div className="my-5 dark:text-gray-300 text-gray-900">
-        <MDXRemoteWrapper {...res.source} />
-      </div>
-
-      <div className="flex justify-between">
-        <button
-          type="button"
-          className={
-            'block mb-10 py-3 relative ' +
-            "after:content-[''] after:bottom-3 after:absolute after:left-0 after:right-0 after:w-0 after:h-1 " +
-            'after:hover:w-full after:transition-all after:bg-gradient-to-tr after:from-red-100 after:to-violet-900 after:hover:h-0.5'
-          }
-        >
-          <Link
-            href="/read"
-            className="text-violet-700 dark:text-violet-400 dark:hover:text-violet-300 hover:text-violet-800 font-normal text-lg"
+    <>
+      <div className="hidden p-8 sm:flex-row max-w-full">css for prose</div>
+      <div className="article-container mt-10">
+        <div>
+          <h1
+            className={
+              `${roboto.className} dark:text-gray-300 text-center text-3xl sm:text-4xl md:text-5xl text-gray-900 relative mt-10 ` +
+              "after:content[''] after:absolute after:w-[200px] after:h-[180px] after:-bottom-20 after:left-32 after:right-0 after:mx-auto " +
+              'after:rounded-full ' +
+              "after:bg-cover after:bg-[url('/gradient.svg')]"
+            }
           >
-            Back to posts
-          </Link>
-        </button>
+            {res.data.title}
+          </h1>
+          <p className="font-bold text-center text-violet-600 dark:text-violet-600 mb-10">{res.data.tags.join('  ')}</p>
+          <span className="dark:text-gray-400 text-gray-600">
+            {res.data.publishedAt} &bull; {res.readingTime} min
+          </span>
+          <hr className="dark:border-gray-600 border-gray-300 mt-2" />
+        </div>
+        <div className="my-5 dark:text-gray-300 text-gray-900 font-sans">
+          <MDXRemoteWrapper {...res.source} />
+        </div>
+
+        <div className="flex justify-between">
+          <button
+            type="button"
+            className={
+              'block mb-10 py-3 relative ' +
+              "after:content-[''] after:bottom-3 after:absolute after:left-0 after:right-0 after:w-0 after:h-1 " +
+              'after:hover:w-full after:transition-all after:bg-gradient-to-tr after:from-red-100 after:to-violet-900 after:hover:h-0.5'
+            }
+          >
+            <Link
+              href="/read"
+              className="text-violet-700 dark:text-violet-400 dark:hover:text-violet-300 hover:text-violet-800 font-normal text-lg"
+            >
+              Back to posts
+            </Link>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
